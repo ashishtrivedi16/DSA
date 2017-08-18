8845e6d72c0a43109f6c9d595994561a56f5e154
Ashish Trivedi <triashish7@gmail.com>
2017-08-18 15:06:21 +0530

    Update stack_using_array.c

--- stack/stack_using_array.c
+++ stack/stack_using_array.c
@@ -1,89 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
 
-int stack[MAX], top = -1;
-void push(int stack[], int val);
-int pop(int stack[]);
-int peek(int stack[]);
-void display(int stack[]);
+void push(int stack[], int* top, int* val);
+void pop(int stack[], int* top);
+void peek(int stack[], int* top);
+void display(int stack[], int* top);
+void empty(int stack[], int* top);
 
-
-void push(int stack[], int val){
-    if (top == MAX -1) {
-        printf("Stack OVERFLOW\n");
+void push(int stack[], int* top, int* val){
+    if (*top == 9) {
+        printf("Stack Overflow!!\n");
     } else {
-        while (1) {
-            printf("Enter value to be inserted: ");
-            scanf("%d", &val);
-            if (val == -1) {
-                break;
-            }
-            top++;
-            stack[top] = val;
-        }
+        printf("Enter data: ");
+        scanf("%d", val);
+        (*top)++;
+        stack[*top] = *val;
     }
 }
 
-int pop(int stack[]){
-    int val;
-    if (top == -1) {
-        printf("Stack UNDERFLOW\n");
-        return -1;
+void pop(int stack[], int* top){
+    if (*top == -1) {
+        printf("Stack Underflow!\n");
     } else {
-        val = stack[top];
-        top--;
-        return val;
+        printf("%d was popped from the stack.\n", stack[*top]);
+        (*top)--;
     }
 }
 
-int peek(int stack[]){
-    if (top == -1) {
-        printf("Stack is empty!!\n");
-        return -1;
+void peek(int stack[], int* top){
+    if (*top == -1) {
+        printf("Empty stack!\n");
     } else {
-        return stack[top];
+        printf("%d\n", stack[*top]);
     }
 }
 
-void display(int stack[]){
-    if (top == -1) {
+void display(int stack[], int* top){
+    for (int i = *top; i >= 0; (i)--) {
+        printf("%d\n", stack[i]);
+    }
+}
+
+void empty(int stack[], int* top){
+    if (*top == -1) {
         printf("Stack is empty!!\n");
     } else {
-        for (int i = top; i >= 0; i--) {
-            printf("%d\n", stack[i]);
-        }
+        printf("Stack has %d elements in it!\n", *top + 1);
     }
 }
 
 int main(void){
-    int val = 0, options;
+    int stack[10];
+    int top = -1, val = 0, options;
     printf("************** Main Menu **************\n");
     printf("1) Push\n");
     printf("2) Pop\n");
     printf("3) Peek\n");
     printf("4) Display\n");
-    printf("5) Exit\n");
+    printf("5) Empty check\n");
+    printf("6) Exit\n");
     printf("Enter your option: ");
     scanf("%d", &options);
 Again:
     switch (options) {
         case 1:
-            printf("Enter -1 to stop inserting values.\n");
-            push(stack, val);
+            push(stack, &top, &val);
             break;
         case 2:
-            val = pop(stack);
-            printf("%d was popped from the stack.\n", val);
+            pop(stack, &top);
             break;
         case 3:
-            val = peek(stack);
-            printf("%d\n", val);
+            peek(stack, &top);
             break;
         case 4:
-            display(stack);
+            display(stack, &top);
             break;
         case 5:
+            empty(stack, &top);
+            break;
+        case 6:
             printf("Bye bye!!\n\n\n");
             break;
         default:
@@ -92,7 +87,7 @@ Again:
     }
     printf("What do you want to do next? ");
     scanf("%d", &options);
-    if (options == 5) {
+    if (options == 6) {
         printf("Bye bye!!\n\n\n");
     } else {
         goto Again;
