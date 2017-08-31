b03303356437320ba8d07590b1a0ac789c3cad73
Ashish Trivedi <triashish7@gmail.com>
2017-08-31 18:28:07 +0530

    Update tower_of_hanoi.c

--- recursion/tower_of_hanoi.c
+++ recursion/tower_of_hanoi.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int count = 1;
 void towerofhanoi(int n, char, char, char);
 
 void towerofhanoi(int n, char source, char aux, char dest){
     if (n == 1) {
-        printf("Moved disk 1 from %c to %c\n", source, dest);
+        printf("Step:%d Moved disk 1 from %c to %c\n", count, source, dest);
         return;
     }
     towerofhanoi(n-1, source, dest, aux);
-    printf("Moved disk %d from %c to %c\n", n, source, dest);
+    count++;
+    printf("Step:%d Moved disk %d from %c to %c\n", count, n, source, dest);
+    count++;
     towerofhanoi(n-1, aux, source, dest);
 }
 
 int main(void){
     int n;
-    printf("Enter number of disks: ");
+    printf("Enter number of disks: \n");
     scanf("%d", &n);
     towerofhanoi(n, 'A', 'B', 'C');
 }
